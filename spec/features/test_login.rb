81e278a78e0a6b0a451481da24a8c547cd145580
Eduardo Lima <e.lima@kainos.com>
2015-08-01 15:57:10 +0100

    added logic for available days

--- spec/features/test_login.rb
+++ spec/features/test_login.rb
@@ -5,7 +5,6 @@ feature 'fff' do
 
 	background do
 		visit config['url']
-		puts "testsetset" + config['title']
 		expect(page).to have_title config['']
 	end	
 
@@ -24,82 +23,85 @@ feature 'fff' do
 
 		page.find('#MainContent_LinkButtonNext').click
 
-			# within :table, 'MainContent_AppointmentDateTime1_Control_Calendar1_Calendar_Calendar1_Calendar1' do
-
-				
-			# end
-
 			available_dates = Array.new
-			days_on_calendar = Array.new
-			days_of_month = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
-			#days_of_month = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,34]
-			
-			# page.all(:table, 'MainContent_AppointmentDateTime1_Control_Calendar1_Calendar_Calendar1_Calendar1').each do |el|
-			  
-				 unavailable = page.all('.aspNetDisabled')
 
-				 myArray = countArrayElements(unavailable)
+			days_of_month = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
+			  
+				 findBookedDays = page.all('.aspNetDisabled')
+
+				 bookedDays = countArrayElements(findBookedDays)
 
 				 inc = 0
 				 for i in days_of_month
-
-				 		puts "day of the month: #{days_of_month[inc]}"
-				 		puts "number of days found: #{myArray.count(days_of_month[inc])}"
 			
-						str = myArray.count(days_of_month[inc])
-				   
-				
+						str = bookedDays.count(days_of_month[inc])
 
-							inc = inc + 1
+						tempDay = getAvailableDays(str, days_of_month[inc])
 
+						if tempDay != nil
+							available_dates.push(tempDay)
+						end
+
+						inc = inc + 1
 
 				 end
-
 
 				 counter = 0
-				 
-				 for i in days_of_month
-				 
-				 	#test = unavailable_dates.include?(days_of_month[counter])
-				 	test = days_of_month[counter]
-				 	
-				 	test2 = unavailable[counter].text
-				
-				 	# puts test
-				 	# puts test2
-
-				 	# puts unavailable_dates[counter].text
-				 	# puts days_of_month[counter]
-				
-				 	# if newArray.include?(test)
-				 	# 	puts "test"
-				 	# else
-				 	# 	puts "echo lo qui"
-				 	# end
-
-				 	 counter = counter + 1	
-
+				 for i in available_dates
+				 	puts available_dates[counter]
+				 	counter +=1
 				 end
-				 
-			# end
-		
-		
-		sleep 10
+
+				 page.find('#MainContent_LinkButtonNext').click
+ 
+
 	end
 
 end
-
 
 def countArrayElements(array)
 		 inc = 0
 		 newArray = []
 		 for i in array
-
-				 		newArray.push(array[inc].text)
-				 		#puts  "element: #{newArray[inc]}  times:  #{newArray.count(newArray[inc])}"
+				 	newArray.push(array[inc].text)
 				 		inc = inc + 1
-
 				 end
+
 		return newArray
 end	
+
+
+def setAugustCalendar()
+
+	 calendar = Array.new
+	 calendar = ["27", "28", "29", "30", "31","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","1","2","3","4","5","6"]
+
+	 return calendar
+
+end
+
+
+def getAvailableDays(str, dayOfMonth)
+
+		if str == 0
+
+			return dayOfMonth
+
+		elsif str == 1
+
+				countDaysOnMontg = setAugustCalendar()
+
+				days = countDaysOnMontg.count(dayOfMonth)
+
+					if days == 2
+
+		  				return dayOfMonth
+
+					end	
+		end
+
+end
+
+
+
 
